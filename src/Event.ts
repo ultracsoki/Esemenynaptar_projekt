982b230bf4335c965db62a8253d07a31a65bfe26
mac <halasi.czalbert@petrik.hu>
2023-11-23 09:46:33 +0100

    Űrlap elkászítése, Event osztály létrehozása

---
+++ src/Event.ts
@@ -0,0 +1,11 @@
+export class Event {
+    id: number;
+    name: string;
+    date: Date;
+    time: Date;
+
+    constructor(id: number,name: string,date: Date)
+    {
+        this
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
 import './style.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
+import './hozzaadas.ts'
