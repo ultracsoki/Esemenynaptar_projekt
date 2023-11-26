f70a0c009798377eef77808025a16482dd46e155
Csontos <csontostibor48@gmail.com>
2023-11-26 18:49:13 +0100

    hozzaadas is donnn

--- src/hozzaadas.ts
+++ src/hozzaadas.ts
@@ -1,22 +1,64 @@
 import { Event } from "./Event";
 //import { AllEvents } from "./Event";
 
+//Sok mindent helyre kéne tenni!!!!!!
+//Lehet elrontottam az egészed de a feltöltés működik
 
-document.getElementById('buttonAdd')!.addEventListener('click',hozzaAd);
+document.getElementById('buttonAdd')!.addEventListener('click',addNewEvent);
 
-function hozzaAd()
-{
-    const name = (document.getElementById('name') as HTMLInputElement).value;
-    const date = (document.getElementById('date') as HTMLInputElement).value;
-    const time = (document.getElementById('time') as HTMLInputElement).value;
-    const allDay = (Boolean)((document.getElementById('allDay') as HTMLInputElement).checked);
-    const reminder = (document.getElementById('reminder') as HTMLInputElement).value;
-    const priority = (document.getElementById('priority') as HTMLInputElement).value;
-    const details = (document.getElementById('details') as HTMLInputElement).value;
 
-    //Validáció
 
-    const newEvent = new Event(0,name,date,time,allDay,reminder,priority,details);
-    console.log(newEvent);
-    alert("Sikeresen Hozzáadtad")
+async function addNewEvent() {
+    const nameInput = document.getElementById('name') as HTMLInputElement;
+    const dateInput = document.getElementById('date') as HTMLInputElement;
+    const timeInput = document.getElementById('time') as HTMLInputElement;
+    const allDayCheckbox = document.getElementById('allDay') as HTMLInputElement;
+    const prioritySelect = document.getElementById('priority') as HTMLSelectElement;
+    const detailsInput = document.getElementById('details') as HTMLInputElement;
+    const reminderInput = document.getElementById('reminder') as HTMLInputElement;
+//validácio
+
+    const name = nameInput.value;
+    const date = dateInput.value;
+    const time = timeInput.value;
+    const allDay = allDayCheckbox.checked;//Már nem False minden esetben
+    const priority = prioritySelect.value;
+    const details = detailsInput.value;
+    const reminder = reminderInput.value
+//ha üresenhagyod meghalsz
+    if (name.trim() === '') {
+        alert('Az esemény nevének kitöltése kötelező.');
+        return; 
+    }
+//egész napos e csóró
+    if (allDay) {
+        timeInput.value = '00:00';
+    } else if (time === '') {
+        alert('Az idő mező kitöltése kötelező, kivéve, ha az esemény egész napos.');
+        return; 
+    }
+//dátum validáció
+    const currentDate = new Date();
+    const eventDate = new Date(date);
+    if (reminder !== '' && new Date(reminder) >= currentDate) {
+        alert('Az emlékeztető csak a jelenlegi dátum előtti dátum lehet.');
+        return; // 
+    }
+
+
+    const newEvent = new Event(0, name, date, time, allDay, priority, reminder, details); // EMBER A SZERVER ADJA AZ ID-t
+
+    const response = await fetch('https://retoolapi.dev/dFqFgC/data', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newEvent),
+    });
+   
+    /*if (response.ok) {
+        console.log('Az esemény hozzáadva a szerverhez.');
+    } else {
+        console.error('Hiba történt az esemény hozzáadása közben.');
+    }*/
 }
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import './hozzaadas.ts'
 import { Event } from './Event.ts'
 
 
+
 document.addEventListener('DOMContentLoaded',init);
 
 async function init()
@@ -16,40 +17,6 @@ async function init()
     {
          throw new Error('Hiba történt a letöltés közben.');
     }
-    let tartalom = await eredmeny.json() as Event[];
-    const addButton = document.getElementById('buttonAdd');
-            addButton!.addEventListener('click', addNewEvent);
-}
-async function addNewEvent() {
-    const nameInput = document.getElementById('name') as HTMLInputElement;
-    const dateInput = document.getElementById('date') as HTMLInputElement;
-    const timeInput = document.getElementById('time') as HTMLInputElement;
-    const allDayCheckbox = document.getElementById('allDay') as HTMLInputElement;
-    const prioritySelect = document.getElementById('priority') as HTMLSelectElement;
-    const detailsInput = document.getElementById('details') as HTMLInputElement;
-    const reminderInput = document.getElementById('reminder') as HTMLInputElement;
-
-    const name = nameInput.value;
-    const date = dateInput.value;
-    const time = timeInput.value;
-    const allDay = allDayCheckbox.checked;//Már nem False minden esetben
-    const priority = prioritySelect.value;
-    const details = detailsInput.value;
-    const reminder = reminderInput.value
-
-    const newEvent = new Event(0, name, date, time, allDay, priority, reminder, details); // EMBER A SZERVER ADJA AZ ID-t
-
-    const response = await fetch('https://retoolapi.dev/dFqFgC/data', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newEvent),
-    });
-
-    /*if (response.ok) {
-        console.log('Az esemény hozzáadva a szerverhez.');
-    } else {
-        console.error('Hiba történt az esemény hozzáadása közben.');
-    }*/
+    
+    
 }
