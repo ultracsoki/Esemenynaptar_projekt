f89616a8662c53d52fe6fc4f488c4921cbaca8c3
Csontos <csontostibor48@gmail.com>
2023-11-29 12:55:33 +0100

    törlés

--- src/esemenyek.ts
+++ src/esemenyek.ts
@@ -65,9 +65,51 @@ function renderEventList(eventList: Event[]) {
                 <p class="card-text">Prioritás: ${event.prioritas}</p>
                 <p class="card-text">Emlékeztető: ${event.emlekezteto}</p>
                 <p class="card-text">Részletek: ${event.reszletek}</p>
+                <button class="btn btn-primary delete-button" data-id="${event.id}">Törlés</button>
+                <button class="btn btn-primary modify-button" data-id="${event.id}">Módosítás</button>
             </div>
         </div>
         `;
     });
-    document.body.appendChild(cardElement);
+}//<--ha kikommenteled akkor ezt tavolitssd el
+   /* document.body.appendChild(cardElement);
+
+    const deleteButtons = document.querySelectorAll('.delete-button');
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            //hasonlo mint az alert csak itt vannak gombok
+            const confirmDelete = window.confirm('Biztosan törölni szeretné ezt az eseményt?');
+
+            if (confirmDelete) {
+                const eventId = button.getAttribute('data-id');
+                deleteEvent(eventId);
+            }
+        });
+    });
+
+   
 }
+
+async function deleteEvent(eventId: string | null) {
+    try {
+        // nem e null az id
+        if (eventId === null) {
+            console.error('Az esemény azonosítója null értékű.');
+            return;
+        }
+
+        const response = await fetch(`https://retoolapi.dev/dFqFgC/data${eventId}`, {
+            method: 'DELETE',
+        });
+
+        if (!response.ok) {
+            throw new Error('Hiba történt az esemény törlése közben.');
+        }
+
+        // Frissites torles utan
+        displayAllEvents();
+    } catch (error) {
+        console.error('Hiba történt az esemény törlése közben:', error);
+    }
+}*/
+
